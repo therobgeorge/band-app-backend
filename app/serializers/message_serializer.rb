814f45266b44ce73e80a5ecefc41ba393f50bbef
therobgeorge <georg1rd@gmail.com>
2021-07-06 13:10:45 -0400

    adds created_at to messages serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :body
+  attributes :id, :user_id, :body, :created_at
 
   belongs_to :conversation
   belongs_to :user
