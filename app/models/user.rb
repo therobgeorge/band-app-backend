c2d0ede39e2fae993abce73a6bcc35a429e1dcae
therobgeorge <georg1rd@gmail.com>
2021-07-08 15:09:59 -0400

    changes user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
-  validates :user_name, presence: true
   validates :email, presence: true
   validates :profile_picture, presence: true
   validates :bio, presence: true
